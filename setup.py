274016e7bf9db160a98c40b1c8aa4a60ef2c54cb
Sunil Abraham <s.abraham@gmail.com>
2015-09-21 15:40:12 -0400

    Dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+requires=[
+    "requests>=2.7.0",
+    "pyquery>=1.2.9"
+]
+
 setup(name='tal.amp',
       version='0.1.0',
       description='AMP for TAL',
       author='Sunil Abraham',
       license='MIT',
+      install_requires=requires,
       packages=[
           'tal',
           'tal.amp'
